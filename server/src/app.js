127ea8ff77f9b04c977ce761cfb3e721bd12682e
Austin Young <austiny255@gmail.com>
2019-02-10 16:42:03 -0500

    beginning expessjs set up

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,19 @@
-console.log("test");
+console.log("test")
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: "test"
+  })
+})
+
+ app.listen(process.env.PORT || 8081)
+ 
