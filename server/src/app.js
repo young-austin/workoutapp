c4685b55451a0dff378eafe672ca1ecb90839f5d
Austin Young <austiny255@gmail.com>
2019-02-12 23:44:41 -0500

    added dependencies and updated port

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,18 @@
-console.log("test")
+console.log('test')
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
 const app = express()
-app.use(morgan('combine'))
+app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/status', (req, res) => {
   res.send({
-    message: "test"
+    message: 'test'
   })
 })
 
- app.listen(process.env.PORT || 8081)
- 
+app.listen(process.env.PORT || 8082)
